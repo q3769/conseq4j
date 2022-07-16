d9e4b05361e36d261c6dd096f5ac48b351970418
Qingtian Wang <wqt.net@gmail.com>
2022-07-16 15:28:11 -0500

    doc

--- README.md
+++ README.md
@@ -46,11 +46,11 @@ In fact, for simplicity and separation of concerns, the default implementation i
 garantee of access order in case of racing conditions in client-side task submission.
 
 It is the API client's responsibility and concern to ensure that tasks are submitted to conseq4j in proper sequence to
-begin with. Fortunately often times, that is naturally the case e.g. when the client is under the management of a
+begin with. Fortunately often times, that is naturally the case, e.g., when the client is under the management of a
 messaging provider running a single caller thread. Otherwise, if the caller is multi-threaded, then the API client needs
-to ensure proper access order among the concurrent caller threads. e.g. This can be as trivial as setting up
+to ensure proper access order among the concurrent caller threads. This can be as trivial as setting up
 a [fair lock](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/locks/ReentrantLock.html#ReentrantLock-boolean-)
-to safeguard the API invocation.
+to safeguard the API invocation; it is a client-side activity nonetheless.
 
 Once the proper task submission sequence is ensured by the API client, it is then conseq4j's concern and responsibility
 that further processing of the tasks is executed in the meaningful order and concurrency as promised.
--- src/main/java/conseq4j/service/ConcurrentSequencerService.java
+++ src/main/java/conseq4j/service/ConcurrentSequencerService.java
@@ -33,15 +33,14 @@ import java.util.concurrent.Future;
  * <p>
  * It may seem counter-intuitive for a concurrent API, but the implementation of conseq4j does not have to be
  * thread-safe. In fact, for simplicity and separation of concerns, the default implementation is not thread-safe in
- * that it provides no garantee of access order in case of racing conditions in task submission.
+ * that it provides no garantee of access order in case of racing conditions in client-side task submission.
  * <p>
  * It is the API client's responsibility and concern to ensure that tasks are submitted to conseq4j in proper sequence
  * to begin with. Fortunately often times, that is naturally the case e.g. when the client is under the management of a
  * messaging provider running a single caller thread. Otherwise, if the caller is multi-threaded, then the API client
- * needs to ensure the proper access order among the concurrent caller threads. e.g. This can be as trivial as setting
- * up a <a
+ * needs to ensure proper access order among the concurrent caller threads. This can be as trivial as setting up a <a
  * href="https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/locks/ReentrantLock.html#ReentrantLock-boolean-">fair
- * lock</a> to safeguard the API invocation.
+ * lock</a> to safeguard the API invocation; it is a client-side activity nonetheless.
  * <p>
  * Once the proper task submission sequence is ensured by the API client, it is then conseq4j's concern and
  * responsibility that further processing of the tasks is executed in the meaningful order and concurrency as promised.
