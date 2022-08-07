cfd342e8c3d944f2cd7fc51a826779c85830e63e
Qingtian Wang <wqt.net@gmail.com>
2022-08-07 11:38:44 -0500

    doc

--- src/main/java/conseq4j/service/StagingConcurrentSequencerService.java
+++ src/main/java/conseq4j/service/StagingConcurrentSequencerService.java
@@ -76,26 +76,26 @@ import java.util.logging.Level;
      * completion-stages of the {@link CompletableFuture} on the same key; i.e. the "main-line" execution.
      * <p>
      * A {@link ConcurrentMap} is employed to keep track of each sequence key's pending task execution stages. In a way,
-     * each map entry represents a sequential executor for all the tasks under the same sequence/entry key; the entry's
-     * value is to hold the most recently added main-line task (completion stage) i.e. the tail of the FIFO task queue
-     * for the same sequence key. Each submitted task will create a new corresponding main-line completion stage which
-     * is queued behind the previous task's completion stage. As part of the same atomic transaction, the newly-created
-     * completion stage, as the tail of the FIFO task queue, also replaces the previous stage as the new value under the
-     * same sequence key in the executor map. As the stages are queued, this new stage will not start executing before
-     * the previous stage completes, and, will have to end executing before the next task's completion stage can start
-     * executing. Such linear progression of the main-line tasks/stages ensures the sequential-ness of task execution
-     * under the same sequence key.
+     * each map entry represents an active sequential executor in-service for all the tasks under the same
+     * sequence/entry key; the entry's value is to hold the most recently added execution task (completion stage) i.e.
+     * the tail of the FIFO task queue of the active executor. With this executor map, an active executor can be located
+     * by its sequence key in case further tasks/stages of the same key need to be queued while previous tasks are
+     * pending execution. Otherwise, if there are no pending tasks (active executor) for this sequence key, a new
+     * entry/executor will be created. Each submitted task will create a new corresponding main-line completion stage
+     * which is either the head (and tail) of a new FIFO task queue of a new executor or queued behind the previous
+     * task's completion stage. In case of the latter, within the same atomic transaction, the newly-created completion
+     * stage, as the tail of the FIFO task queue, also replaces the previous stage as the new value under the same
+     * sequence key in the map. As the stages are queued, this new stage will not start executing before the previous
+     * stage completes, and, will have to end executing before the next task's completion stage can start executing.
+     * Such linear progression of the main-line tasks/stages ensures the sequential-ness of task execution under the
+     * same sequence key.
      * <p>
      * A separate maintenance/cleanup stage is set up to run after the completion of each main-line task/stage. This
-     * maintenance stage checks on the completion status of the most recent main-line task/stage under the same sequence
-     * key, and removes the entire executor entry from the executor map if the checked task/stage is done. While the
-     * checked task/stage is the tail of the task queue, it may or may not be the same stage that triggered this
-     * maintenance check. Unlike the main-line task/stage, the maintenance stage is not part of the main-line execution
-     * task queue; it may remove a completed main-line task/stage (and the entire executor entry) from the map, but does
-     * not disturb the overall sequential-ness of the main-line executions. Meanwhile, as each completed main-line
-     * execution is always triggering an "off-of-band" maintenance/cleanup check, collectively, this ensures that every
-     * main-line task/stage ever put on the executor map is eventually checked for completion and removal; i.e. no entry
-     * will forever linger in the executor map.
+     * maintenance stage will sweep the executor entry off of the map if all tasks of the executor are completed. It
+     * ensures that every executor ever put on the map is eventually removed after all its tasks are done executing;
+     * i.e. no entry will forever linger in the executor map. Unlike a main-line task/stage, a maintenance stage is
+     * never put inside the executor's task queue; it may remove a completed main-line task/stage (and the entire
+     * executor entry) from the map, but does not disturb the overall sequential-ness of the main-line executions.
      */
     @Override public void execute(@NonNull Runnable command, @NonNull Object sequenceKey) {
         this.sequentialExecutors.compute(sequenceKey, (sameSequenceKey, currentExecutionStage) -> {
@@ -108,22 +108,20 @@ import java.util.logging.Level;
                                 command.run();
                                 return null;
                             }, this.executionThreadPool);
-            sweepExecutorIfTailTaskDone(sequenceKey, nextExecutionStage);
+            sweepExecutorIfAllTasksComplete(sequenceKey, nextExecutionStage);
             return nextExecutionStage;
         });
     }
 
     /**
-     * The thread pool to conduct the sweeping maintenance is the default {@link ForkJoinPool#commonPool()}, and cannot
-     * be customized. The check and possible sweep is triggered by the completion of each main-line stage's execution.
-     * This ensures the executor under the same sequence key will always be checked and removed from the executor map at
-     * some point in time, and no executor - after its completion - can linger in the map forever.
+     * The thread pool to conduct the executor sweeping maintenance is the default {@link ForkJoinPool#commonPool()},
+     * and cannot be customized.
      *
      * @param sequenceKey the key whose tasks are sequentially executed
      * @param triggerTask the task/stage that triggers a check and possible sweep of the executor from the map if
      *                    executor's tail task in queue is done at the time of checking
      */
-    private void sweepExecutorIfTailTaskDone(Object sequenceKey, CompletableFuture<?> triggerTask) {
+    private void sweepExecutorIfAllTasksComplete(Object sequenceKey, CompletableFuture<?> triggerTask) {
         triggerTask.whenCompleteAsync(
                 (whateverResult, whateverException) -> sequentialExecutors.computeIfPresent(sequenceKey,
                         (sameSequenceKey, tailTask) -> {
@@ -155,7 +153,7 @@ import java.util.logging.Level;
                         return call(task);
                     }, this.executionThreadPool);
             resultHolder.setFuture(nextExecutionStage);
-            sweepExecutorIfTailTaskDone(sequenceKey, nextExecutionStage);
+            sweepExecutorIfAllTasksComplete(sequenceKey, nextExecutionStage);
             return nextExecutionStage;
         });
         return new MinimalFuture<>(resultHolder.getFuture());
