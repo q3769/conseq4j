b1dabe93da8934ba47d37eb56a17ec79f7479737
q3769 <wqt.net@gmail.com>
2023-01-07 16:44:50 -0600

    refactor

--- src/main/java/conseq4j/execute/ConseqExecutor.java
+++ src/main/java/conseq4j/execute/ConseqExecutor.java
@@ -28,7 +28,6 @@ import lombok.NonNull;
 import lombok.ToString;
 import lombok.experimental.Delegate;
 
-import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.concurrent.*;
 
@@ -74,81 +73,72 @@ public final class ConseqExecutor implements ConcurrentSequencingExecutor {
         }
     }
 
-    @Nullable
-    private static Object run(@NonNull Runnable command) {
-        command.run();
-        return null;
-    }
-
     int estimateActiveExecutorCount() {
         return this.sequentialExecutors.size();
     }
 
     /**
-     * Sequential execution of tasks under the same/equal sequence key is achieved by linearly processing the
-     * completion-stages of the {@link CompletableFuture} on the same key; i.e. the "main-line" execution.
-     * <p>
-     * A {@link ConcurrentMap} is employed to keep track of each sequence key's pending tasks. Each map entry represents
-     * an active sequential executor in-service for all the tasks under the same sequence/entry key; the entry's value
-     * is to hold the most recently added task (completion stage), i.e. the tail of the FIFO task queue of the active
-     * executor. With this executor map, an active executor can be located by its sequence key so that further
-     * tasks/stages of the same key can be queued behind the previous task(s) of the same executor. If no active
-     * executor exists in the map for the submitted task's sequence key, a new entry/executor will be created. Each
-     * submitted task will create a new corresponding main-line completion stage which is always put on the executor map
-     * - either as the head (and tail) of a new executor's task queue, or the tail of an existing executor's task queue.
-     * This new stage will not start executing before the previous stage completes, and, will have to complete execution
-     * before the next task's completion stage can start executing. Such linear progression of the main-line
-     * tasks/stages ensures the sequential-ness of task execution under the same sequence key.
-     * <p>
-     * A separate maintenance/cleanup stage is set up to run after the completion of each main-line task/stage. This
-     * maintenance stage will sweep the executor entry off of the map if all tasks of the executor are completed. It
-     * ensures that every executor ever put on the map is eventually removed after all its tasks are done executing;
-     * i.e. no entry will forever linger in the executor map. Unlike a main-line task/stage, a maintenance stage is
-     * never put in a task queue or the executor map, and has no effect on the overall sequential-ness of the main-line
-     * executions.
-     *
      * @param command     the command to run asynchronously in proper sequence
-     * @param sequenceKey the key that the command should be queued behind such that all commands of the same key are
-     *                    sequentially executed
+     * @param sequenceKey the key under which this task should be sequenced
+     * @see ConseqExecutor#submit(Callable, Object)
      */
     @Override
     public Future<Void> execute(@NonNull Runnable command, @NonNull Object sequenceKey) {
-        CompletableFuture<?> commandStage = this.sequentialExecutors.compute(sequenceKey,
-                (sameSequenceKey, currentExecutionStage) -> (currentExecutionStage == null) ?
-                        CompletableFuture.runAsync(command, this.workerThreadPool) :
-                        currentExecutionStage.handleAsync((currentResult, currentException) -> run(command),
-                                this.workerThreadPool));
-        sweepExecutorIfAllTasksComplete(sequenceKey, commandStage);
-        return new SimpleFuture<>(commandStage);
+        return submit(Executors.callable(command, null), sequenceKey);
     }
 
     /**
+     * Sequential execution of tasks under the same/equal sequence key is achieved by linearly chaining/queuing the
+     * task/work stages of the same key, leveraging the {@link CompletableFuture} API.
+     * <p>
+     * A {@link ConcurrentMap} is employed to keep track of each sequence key's pending tasks status. Each map entry
+     * represents an active sequential executor in-service for all the tasks under the same sequence key; the entry's
+     * value is to hold the most recently added task as the tail of the FIFO task queue of the active executor. With
+     * this executor map, an active executor can be located by its sequence key so that a subsequent task of the same
+     * key can be queued behind the previous task. If no active executor exists in the map for the submitted task's
+     * sequence key, a new entry/executor will be created. Before added to the tail of the task queue, each task will be
+     * converted to a new corresponding work stage (of type {@link CompletableFuture}). If there already exists an
+     * entry/work stage of the same sequence key in the map, the current work stage will be set to execute behind the
+     * existing work stage and replace the existing stage as the tail of the task queue in the map entry. This new work
+     * stage will not start executing before the previous stage completes, and will have to finish executing before the
+     * next task's work stage can start executing. Such linear progression of the work tasks/stages ensures the
+     * sequential-ness of task execution under the same sequence key.
+     * <p>
+     * A separate maintenance task/stage is set up to run after the completion of each work task/stage. Under the same
+     * sequence key, this maintenance stage will locate the executor entry in the map, and sweep the entire executor
+     * entry off of the map if the entry's work stage is complete; otherwise if the work stage is not complete, the
+     * maintenance stage does nothing; the incomplete work stage stays in the map under the same sequence key, ready to
+     * be trailed by the next work stage. This clean up maintenance ensures that every work stage ever put on the map is
+     * eventually removed as long as every work stage runs to complete. Unlike a work task/stage, a maintenance
+     * task/stage is never added in the task queue or the executor map, and has no effect on the overall sequential-ness
+     * of the work stage executions.
+     *
      * @param task        the task to be called asynchronously with proper sequence
      * @param sequenceKey the key under which this task should be sequenced
-     * @return future result of the task. Not downcast-able.
-     * @see ConseqExecutor#execute(Runnable, Object)
+     * @return future result of the task, not downcast-able.
      */
     @Override
+    @SuppressWarnings("unchecked")
     public <T> Future<T> submit(@NonNull Callable<T> task, @NonNull Object sequenceKey) {
-        CompletableFuture<?> taskStage = this.sequentialExecutors.compute(sequenceKey,
+        CompletableFuture<?> workStage = this.sequentialExecutors.compute(sequenceKey,
                 (sameSequenceKey, currentExecutionStage) -> (currentExecutionStage == null) ?
                         CompletableFuture.supplyAsync(() -> call(task), this.workerThreadPool) :
                         currentExecutionStage.handleAsync((currentResult, currentException) -> call(task),
                                 this.workerThreadPool));
-        sweepExecutorIfAllTasksComplete(sequenceKey, taskStage);
-        return new SimpleFuture<>(taskStage);
+        sweepExecutorIfTailTaskComplete(sequenceKey, workStage);
+        return new MinimalFuture<>((Future<T>) workStage);
     }
 
     /**
      * When trigger task is complete, check and de-list the executor entry if all is complete
      *
-     * @param sequenceKey the key whose tasks are sequentially executed
-     * @param triggerTask the task/stage that triggers a check and possible sweep of the executor from the map if
-     *                    executor's tail task in queue is done at the time of checking
+     * @param sequenceKey  the key whose tasks are sequentially executed
+     * @param sweepTrigger the task/stage that triggers a check and possible sweep of the executor from the map if
+     *                     executor's tail task in queue is done at the time of checking
      */
-    private void sweepExecutorIfAllTasksComplete(Object sequenceKey, @NonNull CompletableFuture<?> triggerTask) {
-        triggerTask.whenCompleteAsync((anyResult, anyException) -> sequentialExecutors.computeIfPresent(sequenceKey,
-                (sameSequenceKey, tailTask) -> tailTask.isDone() ? null : tailTask), ADMIN_THREAD_POOL);
+    private void sweepExecutorIfTailTaskComplete(Object sequenceKey, @NonNull CompletableFuture<?> sweepTrigger) {
+        sweepTrigger.whenCompleteAsync((anyResult, anyException) -> sequentialExecutors.computeIfPresent(sequenceKey,
+                (sameSequenceKey, tailWorkStage) -> tailWorkStage.isDone() ? null : tailWorkStage), ADMIN_THREAD_POOL);
     }
 
     /**
@@ -157,12 +147,11 @@ public final class ConseqExecutor implements ConcurrentSequencingExecutor {
      *
      * @param <V> type of result held by the Future
      */
-    private static final class SimpleFuture<V> implements Future<V> {
+    private static final class MinimalFuture<V> implements Future<V> {
         @Delegate private final Future<V> future;
 
-        @SuppressWarnings("unchecked")
-        SimpleFuture(@NonNull Future<?> future) {
-            this.future = (Future<V>) future;
+        MinimalFuture(@NonNull Future<V> future) {
+            this.future = future;
         }
     }
 
